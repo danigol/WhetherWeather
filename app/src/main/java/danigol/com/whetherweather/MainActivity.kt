cedd7ae13569b31d16237602faeb45979ee33d3a
Danielle Golinsky <daniellegolinsky@iheartmedia.com>
2018-11-13 16:03:53 -0500

    List adapter.

--- app/src/main/java/danigol/com/whetherweather/MainActivity.kt
+++ app/src/main/java/danigol/com/whetherweather/MainActivity.kt
@@ -2,13 +2,27 @@ package danigol.com.whetherweather
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val items = listOf(
+            "Mon 11/12 - Partly Cloudy - 50/42",
+            "Tue 11/13 - Cloudy - 49/39",
+            "Wed 11/14 - Partly Sunny - 51/42",
+            "Thu 11/15 - Partly Cloudy - 49/42",
+            "Fri 11/16 - Partly Sunny - 55/39",
+            "Sat 11/17 - Sunny - 60/49",
+            "Sun 11/18 - Rain - 50/37"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        welcome_message.text = "Hello, Kotlin!"
+
+        val forecastList = findViewById<RecyclerView>(R.id.forecast_list) as RecyclerView
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
     }
 }
